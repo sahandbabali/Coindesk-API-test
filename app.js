730e4c4fe6d62a3b2e056c7e70a24d4e32c39d8d
Sahand Babali <42813816+sahandbabali@users.noreply.github.com>
2021-05-06 03:41:50 +0430

    Add files via upload

--- app.js
+++ app.js
@@ -1,57 +1,51 @@
+window.onload = getdata;
 
+document.getElementById("fetchbut").addEventListener("click", getdata );
 
-document.getElementById("fetchbut").addEventListener("click", function() {
-      fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => response.json())
-      .then(data => 
-            {
-                  console.log(data)
-                  console.log()
-
-                  document.getElementById("results").innerHTML="";
-
-                  document.getElementById("results").innerHTML += 
-                  `
-                  <h1>${data.chartName}</h1>
-                  <p>${data.time.updated}</p>
-
-
-
-                  <div class="row alert alert-primary" role="alert">
-                  <div class="col">
-                  ${data.bpi.USD.code}${data.bpi.USD.symbol}:
-                  </div>
-                  <div class="col">
-                  ${data.bpi.USD.rate}
-                  </div>
-                  </div>
-
-                  <div class="row alert alert-primary" role="alert">
-                  <div class="col">
-                  ${data.bpi.EUR.code}${data.bpi.EUR.symbol}:
-                  </div>
-                  <div class="col">
-                  ${data.bpi.EUR.rate}
-                  </div>
-                  </div>
-
-
-
-
-
-                  `
-
-
-
-                  
-            }
-            );
-
-
-    });
-
-
-
-
-
-
+function getdata() {
+      fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          console.log();
+    
+          document.getElementById("results").innerHTML = "";
+    
+          document.getElementById("results").innerHTML += `
+                      <h1>${data.chartName}</h1>
+                      <p>${data.time.updated}</p>
+    
+    
+    
+    
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Currency</th>
+          <th scope="col">Price</th>
+    
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>${data.bpi.USD.code}${data.bpi.USD.symbol}</td>
+          <td>${data.bpi.USD.rate}</td>
+      
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>${data.bpi.EUR.code}${data.bpi.EUR.symbol}</td>
+          <td>${data.bpi.EUR.rate}</td>
+     
+        </tr>
+    
+      </tbody>
+    </table>
+    
+               
+    
+                      `;
+        });
+    }
