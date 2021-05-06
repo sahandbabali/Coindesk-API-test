a6ddcebe103ad99f7612c27c061b512821160584
Sahand Babali <42813816+sahandbabali@users.noreply.github.com>
2021-05-06 05:02:41 +0430

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+var oldusp;
+var oldeup;
+
+var newusp;
+var neweup;
+
+
 window.onload = getdata;
 
-document.getElementById("fetchbut").addEventListener("click", getdata );
+window.setInterval(getdata, 5000);
 
 function getdata() {
-      fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          console.log();
-    
-          document.getElementById("results").innerHTML = "";
-    
-          document.getElementById("results").innerHTML += `
+  fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then((response) => response.json())
+    .then((data) => {
+       newusp = data.bpi.USD.rate;
+       neweup = data.bpi.EUR.rate;
+
+      
+
+      document.getElementById("results").innerHTML = "";
+
+      document.getElementById("results").innerHTML += `
                       <h1>${data.chartName}</h1>
                       <p>${data.time.updated}</p>
     
@@ -30,14 +39,14 @@ function getdata() {
       <tbody>
         <tr>
           <th scope="row">1</th>
-          <td>${data.bpi.USD.code}</td>
-          <td>${data.bpi.USD.rate}${data.bpi.USD.symbol}</td>
+          <td  >${data.bpi.USD.code}</td>
+          <td ><span id="usdtable" >${newusp}  ${data.bpi.USD.symbol}</span></td>
       
         </tr>
         <tr>
           <th scope="row">2</th>
-          <td>${data.bpi.EUR.code}</td>
-          <td>${data.bpi.EUR.rate}${data.bpi.EUR.symbol}</td>
+          <td >${data.bpi.EUR.code}</td>
+          <td ><span id="euptable">${neweup}  ${data.bpi.EUR.symbol}</span></td>
      
         </tr>
     
@@ -47,5 +56,34 @@ function getdata() {
                
     
                       `;
-        });
-    }
+
+
+
+                      // compare old and new values
+if (newusp != oldusp) {
+  document.getElementById("usdtable").style.backgroundColor = "green";
+  document.getElementById("usdtable").style.color = "white";
+  setTimeout(() => {  document.getElementById("usdtable").style.backgroundColor = "white";
+  document.getElementById("usdtable").style.color = "black";
+}, 1000);
+
+}
+
+if (neweup != oldeup) {
+  document.getElementById("euptable").style.backgroundColor = "green";
+  document.getElementById("euptable").style.color = "white";
+  setTimeout(() => {  document.getElementById("euptable").style.backgroundColor = "white";
+  document.getElementById("euptable").style.color = "black";
+}, 1000);
+}
+
+
+  oldusp = newusp;
+  oldeup = neweup;
+
+
+
+    });
+
+
+}
